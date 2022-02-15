8d49d35abff5ebbaf2b7f68d6277cf56ea45f0d3
hposedev <hposedev@gmail.com>
2022-02-15 18:00:42 +0100

    added new colors for Light and Dark Theme

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -6,7 +6,7 @@ import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,20 +16,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-         val tipPercentage = when(selectedId){
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost==null || cost == 0.0){
+            displayTip(0.0)
+            return
+        }
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
              R.id.option_twenty_percent-> 0.20
              R.id.option_eighting_percent-> 0.18
              else-> 0.15
          }
         var tip = tipPercentage * cost
-        val roundUp = binding.roundUpSwitch.isChecked
 
-        if (roundUp){
+
+        if (binding.roundUpSwitch.isChecked){
             tip = kotlin.math.ceil(tip)
         }
-        val formatedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount, formatedTip)
+        displayTip(tip)
+    }
+    private fun displayTip(tip : Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
